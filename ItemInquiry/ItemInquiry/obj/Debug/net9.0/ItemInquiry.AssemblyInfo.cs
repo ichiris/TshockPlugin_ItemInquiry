67aa32ad0d84f07eb787e51e3c8b6316d9b82ae4
ichiris <ichiris@outlook.com>
2026-04-02 20:35:35 +0800

    new commit

--- ItemInquiry/ItemInquiry/obj/Debug/net9.0/ItemInquiry.AssemblyInfo.cs
+++ ItemInquiry/ItemInquiry/obj/Debug/net9.0/ItemInquiry.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemInquiry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5976d7400c1d00d8079b6baaca1bfa34390ec6fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd477ad589a7006d88353199f72c00ce1e6c248")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemInquiry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemInquiry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
